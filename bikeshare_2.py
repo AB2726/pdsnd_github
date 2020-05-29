32d68f28c5877aca9da094bac63b85ba2d4afa05
Archie_Best <ab2726@att.com>
2020-05-29 15:47:41 -0500

    changes made to US to United States changes made to remove !

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -14,8 +14,10 @@ def get_filters():
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        UPDATED US to United States
+        Removed !
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello Let\'s explore some United States bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
 
